5e8216cede188d8b0cfb362f16f17ccae9271e0c
Aaron N. Brock <contact@aaronnbrock.com>
2023-05-06 14:08:26 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>Home Page</h1>
+</section>
+
+<style>
+	h1 {
+		width: 100%;
+		height: 100vh;
+		line-height: 100vh;
+	}
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About this app</h1>
+</div>
+
+<style>
+	h1 {
+		width: 100%;
+		height: 100vh;
+		line-height: 100vh;
+	}
+</style>
